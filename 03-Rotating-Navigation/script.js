687daa14d27456f94dd0453a6b6aa0ab362131b9
Sarai <saraichuchi@gmail.com>
2022-04-28 20:18:13 -0400

    commit

--- 03-Rotating-Navigation/script.js
+++ 03-Rotating-Navigation/script.js
@@ -5,4 +5,4 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
-// buttons are not showing or working with the class that should change UPDATE IT WAS CAUSE THERE WAS A SPACE BETWEEN ROTATO AND (-70 DEG) 
+// buttons are not showing or working with the class that should change UPDATE IT WAS CAUSE THERE WAS A SPACE BETWEEN ROTATO AND (-70 DEG)  
