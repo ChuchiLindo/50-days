953666a518529babb170d7bfcc7700248495b57a
Sarai <saraichuchi@gmail.com>
2022-04-30 13:55:57 -0400

    commit

---
+++ 08-Form-Wave-Animation/script.js
@@ -0,0 +1,12 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    // inner text getting each letter
+    label.innerHTML = label.innerText
+    // split into an array splits the letteres into an array
+        .split('')
+        // manipluate trun it into an array into some thing else we want a letter with a span arrount it to add a transition 
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        // turn it back into a string
+        .join('')
+})
